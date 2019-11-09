54931dbd1aa4dc02d43645e33df8c4a8bc75d0a3
tomaszkudla <tomaszjankudlla@gmail.com>
2019-11-09 15:10:26 +0100

    Add project files.

---
+++ DummyFile/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DummyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ParseParams(args, out string path, out int sizeMB);
+                if (sizeMB <= 0)
+                {
+                    throw new ArgumentException("File size should be integer > 0 MB");
+                }
+
+                GenerateFileWithRandom(path, sizeMB);
+                Console.WriteLine("Done");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ParseParams(string[] args, out string path, out int sizeMB)
+        {
+            path = args.ElementAtOrDefault(0);
+            int.TryParse(args.ElementAtOrDefault(1), out sizeMB);
+        }
+
+        private static void GenerateFileWithRandom(string path, int sizeMB)
+        {
+            var count = (sizeMB * 1024 * 1024);
+            using (var streamWriter = new StreamWriter(path))
+            {
+                Random random = new Random();
+                for (int i = 0; i < count; i++)
+                {
+                    var num = random.Next(0, 127);
+                    var ch = (char)num;
+                    streamWriter.Write(ch);
+                }
+            }
+        }
+    }
+}
